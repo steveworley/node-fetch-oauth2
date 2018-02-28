dcc2b1443af1a12d3aca754b8c4a139dd6835647
Steve Worley <steven.worley@acquia.com>
2018-02-28 14:04:24 +1000

    Update project name.

--- test/fetch.js
+++ test/fetch.js
@@ -1,20 +1,20 @@
 import { assert, expect } from 'chai'
-import AcquiaFetch from '../src/index'
+import fetchWithConfig from '../src/index'
 
-describe('Acquia Oauth Fetch', () => {
+describe('Node Fetch Oauth', () => {
 
   let fetch;
   const host = 'https://httpbin.org'
 
   it('should automatically set the host', () => {
-    fetch = AcquiaFetch({ host })    
+    fetch = fetchWithConfig({ host })    
     fetch('/get').then(res => {
       assert(res.url === `${host}/get`, 'Host could not be set.')
     }).catch(err => assert.fail('Error', 'Response', err))
   })
 
   it('should allow custom headers to be appended', () => {
-    fetch = AcquiaFetch({ host, headers: [['Accept', 'application/json']] }, (next) => {
+    fetch = fetchWithConfig({ host, headers: [['Accept', 'application/json']] }, (next) => {
       return config => {
         next(config.then(config => {
           assert.nestedInclude(config.opts.headers, {'Accept': 'application/json'})
@@ -28,11 +28,11 @@ describe('Acquia Oauth Fetch', () => {
   it('should allow custom middlewares', () => {
     let middleware = false
 
-    fetch = AcquiaFetch({ host }, (next) => {
+    fetch = fetchWithConfig({ host }, (next) => {
       middleware = true
       return config => next(config)
     })
-    
+
     fetch('/get').then(res => {
       assert(middleware, true, 'The middleware was not called')
     })
